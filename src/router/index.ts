62b3c577983b94f716bd8212642d22b5eb5b0aff
RWG <1940449715@qq.com>
2024-07-29 09:47:08 +0800

    a

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import homePage from "@/views/homePage.vue"
 
 const router = createRouter({
+  
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
